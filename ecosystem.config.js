9e9f77cd2b5fc74fcb633e5a61b2d487aec8610a
Mohak Talwar <mohak.talwar@netcomlearning.com>
2025-03-22 13:39:54 +0530

    sticky mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,13 @@
 module.exports = {
 	apps: [
 		{
-			name: "webrtc-service", // Application name for PM2
-			script: "server.js", // Entry point of your app (change as needed)
-			instances: "6", // Scales to all available CPU cores
-			exec_mode: "cluster", // Enables cluster mode
-			watch: false, // Set to true if you want PM2 to watch file changes (useful in development)
-			max_memory_restart: "1000G", // Restarts the app if it uses more than 500 MB of memory
+			name: "webrtc-service",
+			script: "server.js",
+			instances: "6",
+			exec_mode: "cluster",
+			sticky: true, // Enable sticky sessions in cluster mode
+			watch: false,
+			max_memory_restart: "1000G",
 			env_file: "./.env",
 		},
 	],
